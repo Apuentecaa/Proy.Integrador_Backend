7cc13fd9a85f234b079be5bc07a9160464acc043
Anthony <anthonyjhoanpuentecarpio@gmail.com>
2026-05-12 22:15:28 -0500

    feat: configuracion de Spring Security y encriptacion BCrypt

--- src/main/java/com/proyectointegrador/backend/application/services/AuthService.java
+++ src/main/java/com/proyectointegrador/backend/application/services/AuthService.java
@@ -1,27 +1,30 @@
-
 package com.proyectointegrador.backend.application.services;
 
 import com.proyectointegrador.backend.domain.model.User;
 import com.proyectointegrador.backend.domain.ports.in.LoginUseCase;
 import com.proyectointegrador.backend.domain.ports.out.UserRepositoryPort;
+import org.springframework.security.crypto.password.PasswordEncoder; // Nuevo import
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
 
 @Service
 public class AuthService implements LoginUseCase {
 
     private final UserRepositoryPort userRepositoryPort;
+    private final PasswordEncoder passwordEncoder; // Inyectamos el encriptador
 
-    public AuthService(UserRepositoryPort userRepositoryPort) {
+    public AuthService(UserRepositoryPort userRepositoryPort, PasswordEncoder passwordEncoder) {
         this.userRepositoryPort = userRepositoryPort;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public boolean login(String username, String password) {
         Optional<User> user = userRepositoryPort.findByUsername(username);
         
-        // Validación básica (luego implementaremos BCrypt para seguridad)
-        return user.isPresent() && user.get().getPassword().equals(password);
+        // Comparamos el texto plano con el hash de la DB
+        return user.isPresent() && passwordEncoder.matches(password, user.get().getPassword());
     }
 }
